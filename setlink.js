f44137ff214562d6b0efc6022f5fe6905e256eb0
dipuakhando2001 <121944271+dipuakhando2001@users.noreply.github.com>
2023-02-14 14:44:30 +0600

    Update setlink.js

--- setlink.js
+++ setlink.js
@@ -6,16 +6,16 @@ const userInput = readline.createInterface({
 });
 console.log("");
 console.log("....................................................");
-console.log("       This Tool Present by Learning Journey        ");
+console.log("       This Tool Present by Md Dipu Ahmed       ");
 console.log("````````````````````````````````````````````````````");
 console.log(" Subscribe My Youtube Channel For Link Update       ");
-console.log(" Youtube:https://www.youtube.com/@learningjourneybd ");
+console.log(" Youtube:https://www.youtube.com/@dipuahmed990 ");
 console.log("____________________________________________________");
 console.log("");
 userInput.question("Enter your link:~$ ", (value) => {
   console.log(value);
   fs.writeFile("link.js", `const offerLink = "${value}";`, function (err) {
-    console.log("Link set seccussful.");
+    console.log("Link set seccussful add .");
     console.log("Now you ready to upload this file...");
     process.exit();
   });
